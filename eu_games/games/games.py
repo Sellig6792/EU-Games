ed430f8d2ef844f3cb043a2fdbf0cef10abdaabe
Sellig6792 <sellig.dev@gmail.com>
2022-03-12 11:58:54 +0100

    Choix du niveau pour le jeu choisi

--- eu_games/games/games.py
+++ eu_games/games/games.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
-games_bp = Blueprint('games_bp', __name__, static_folder='static', template_folder='templates')
+games_bp = Blueprint('games', __name__, static_folder='static', static_url_path='static',
+                     template_folder='templates')
 
 
 @games_bp.route('/')
@@ -8,6 +9,9 @@ def games():
     return "This is an example app"
 
 
-@games_bp.route('/capitals')
-def capitals():
-    return render_template('')
+@games_bp.route('/capitals', defaults={'level': None})
+@games_bp.route('/capitals/<int:level>')
+def capitals(level: int = None):
+    if level is None:
+        return render_template('level_choice.html', game='games.capitals')
+    return render_template('capitals.html', level=level)
