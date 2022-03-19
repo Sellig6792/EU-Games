d2cca8561822888424dd813460a2e0a60455a4ff
Sellig6792 <sellig.dev@gmail.com>
2022-03-19 17:32:14 +0100

    Fix pour Heroku

--- eu_games/main.py
+++ eu_games/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from .games.games import games
 
 app = Flask(__name__)
+app.register_blueprint(games, url_prefix='/games')
 
 
 @app.route('/')
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 from eu_games.main import app
-from eu_games.games.games import games
 from os import environ
 
 if __name__ == "__main__":
-    app.register_blueprint(games, url_prefix='/games')
     if environ.get("ENV") != "PROD":
         app.run(host='0.0.0.0', debug=True)
     else:
