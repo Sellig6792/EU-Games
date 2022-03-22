5f40770ba546b9963eaaf097167f8658303bd421
Sellig6792 <sellig.dev@gmail.com>
2022-03-22 17:45:59 +0100

    Conseil de tenir l'appareil à la verticale

---
+++ eu_games/games/static/tip_orientation.js
@@ -0,0 +1,14 @@
+/* Conseil de tenir l'appareil verticalement */
+function onResizeEvent() {
+    if (window.innerHeight < window.innerWidth) {
+        alert("Il est conseillé de tenir l'appareil à la verticale.");
+    }
+}
+
+if (window.addEventListener) {  // all browsers except IE before version 9
+    window.addEventListener("resize", onResizeEvent, true);
+} else {
+    if (window.attachEvent) {   // IE before version 9
+        window.attachEvent("onresize", onResizeEvent);
+    }
+}
